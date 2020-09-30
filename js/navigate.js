464db7f6ec04e3af8d45325d1d95ce1b959b9fff
leshwebdev <71425034+leshwebdev@users.noreply.github.com>
2020-09-30 11:21:26 +0300

    Update navigate.js

--- js/navigate.js
+++ js/navigate.js
@@ -1,4 +1,7 @@
 $('.ui-select').on('change',function(){
     var value = $(this).val();
-    location.href = 'index-'+ value +'.html'; //or .php, etc. This will go to a page called en.html
+    if (value != ''){
+        location.href = 'index-'+ value +'.html'; //or .php, etc. This will go to a page called en.html
+    } else {
+        location.href = 'index.html'
     });
